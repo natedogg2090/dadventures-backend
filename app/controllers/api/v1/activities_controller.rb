4383f3967a5ccbd317893464cb59fbd549b695ef
Nate Dietz <nsdietz@gmail.com>
2021-04-05 22:14:44 +0100

    created activities index in activities controller

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::ActivitiesController < ApplicationController
+
+    def index
+        activities = Activity.all
+
+        render json: activities, status: 200
+    end
 end
+ 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Activity.create(name: "Roasting Marshmallows by the Campfire", description: "Roasting marshmallows by the campfire is a time honored tradition that has been passed on for generations.", duration: 60, what_you_need: "Marshmallows, Chocolate Bars, Graham Crackers, Long skewers", what_to_do: "Assuming the campfire already exits, 1. Attach the marshmallow to the end of the skewer and begin to roast the marshmallow over the fire. 2. Once the marshmallow is a light, golden brown you can remove the marshmallow from the fire. 3. Place on top of graham cracker and chocolate bar. 4. Place the other half of the graham cracker on top of the hot marshmallow and pull out the skewer. 5. Enjoy!")
+
+Activity.create(name: "Make a walking stick", description: "Easy activity for a walk in the woods", duration: 10, what_you_need: "A stick taller than your child, penknife, saw", what_to_do: "1. Keep your eyes peeled for long straight stick. 2. Ideally the diameter is not greater than 1-2 inches. 3. Cut the stick at the base.")
+
+Activity.create(name: "Watching wildlife after dark", description: "Experiencing nature in its truest form is to experience all elements it has to offer, which includes wildlife.", duration: 60, what_you_need: "All your senses, a torch/headtorch", what_to_do: "1. Wait for the sun to set. 2. Dress warm. 3. Go for a walk.")
