51583a478c43048f34c06248bfa1f1f059f38f3f
Nate Dietz <nsdietz@gmail.com>
2021-04-15 18:42:19 +0100

    refactored Memories and Users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,5 @@
 class Api::V1::UsersController < ApplicationController
 
-    def index
-        users = User.all
-
-        render json: UserSerializer.new(users), status: 200
-    end
-
     def show
         user = User.find(params[:id])
 
