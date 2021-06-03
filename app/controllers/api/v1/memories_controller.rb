96ea3ad3c6accc7f13ebefe2082eefbef41f3259
Nate Dietz <nsdietz@gmail.com>
2021-06-03 23:35:46 +0100

    created index, show and create routes

--- app/controllers/api/v1/memories_controller.rb
+++ app/controllers/api/v1/memories_controller.rb
@@ -1,3 +1,29 @@
 class Api::V1::MemoriesController < ApplicationController
+    def index
+        memory = Memory.all
 
+        render json: MemorySerializer.new(memory), status: 200
+    end
+
+    def show
+        memory = Memory.find(params[:id])
+
+        render json: MemorySerializer.new(memory), status: 200
+    end
+
+    def create
+        memory = Memory.new(memory_params)
+
+        if memory.save
+            render json: MemorySerializer.new(memory), status: 200
+        else
+            render json: {status: 'error', message: 'Unable to create memory.'}
+        end
+    end
+
+    private
+
+    def memory_params
+        params.require(:memory).permit(:description)
+    end
 end
