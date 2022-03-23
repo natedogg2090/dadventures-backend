304708b45b2adb273698def0b4cabe4a3d78a8d1
Nate Dietz <nsdietz@gmail.com>
2022-03-23 20:44:32 +0000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_11_150559) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.text "description"
